6d9a2f2cea45927bac8c9d39b10ab26bf7796f62
Justin Scott <fligtar@gmail.com>
2009-06-02 07:29:48 +0000

    Converting some more tabs to spaces

--- bugstats/js/shiny.js
+++ bugstats/js/shiny.js
@@ -1,30 +1,30 @@
 // Starts backfetch request
 function backfetch() {
-	$('#refresh-trigger').hide();
-	$('#backfetching').slideDown();
-	$.ajax({
-		url: "backfetch.php?project=" + projectName,
-		cache: false,
-		success: function() {
-			$('#backfetching').slideUp();
-			$('#backfetched').slideDown();
-		}
-	});
+    $('#refresh-trigger').hide();
+    $('#backfetching').slideDown();
+    $.ajax({
+        url: "backfetch.php?project=" + projectName,
+        cache: false,
+        success: function() {
+            $('#backfetching').slideUp();
+            $('#backfetched').slideDown();
+        }
+    });
 }
 
 // Highlights user boxes with the selected field
 function highlight(field) {
-	$('#highlight-' + field).toggleClass('on').blur();
-	$('#highlight-' + field + ' a').blur();
-	
-	$('#content').toggleClass('highlight-' + field);
+    $('#highlight-' + field).toggleClass('on').blur();
+    $('#highlight-' + field + ' a').blur();
+    
+    $('#content').toggleClass('highlight-' + field);
 }
 
 // Goes to selected project
 function selectProject() {
-	var project = $('#project-selector').val();
-	
-	if (project != '') {
-		window.location = './' + project;
-	}
+    var project = $('#project-selector').val();
+    
+    if (project != '') {
+        window.location = './' + project;
+    }
 }
