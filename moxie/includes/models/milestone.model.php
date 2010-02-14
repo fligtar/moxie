1b2860b012a510fc3aa48df7786ddb6e1dbbc82c
Justin Scott <fligtar@gmail.com>
2010-02-13 19:03:29 -0800

    removing non-bugstats things from the bugstats branch

--- moxie/includes/models/milestone.model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class MilestoneModel extends Model {
-    public $table = 'milestones';
-    
-
-}
-
-?>
--- moxie2/templates/default/index.template.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($vars['products'])) {
-    echo '<ul>';
-    foreach ($vars['products'] as $product) {
-        echo '<li><a href="'.$this->getBaseURL().'/'.$product['url'].'">'.$product['name'].'</a></li>';
-    }
-    echo '</ul>';
-}
-?>
